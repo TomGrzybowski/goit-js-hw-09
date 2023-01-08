ddea1d8eefb29304132a2cb9482916a04ca3a65a
TomekGrzybek <t.grzybowski94@gmail.com>
2023-01-08 17:26:11 +0100

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+let colorSwitcher = null;
 
+startButton.addEventListener('click', handleStart);
+stopButton.addEventListener('click', handleStop);
+stopButton.setAttribute('disabled', '');
+
+function handleStart(e) {
+  startButton.setAttribute('disabled', '');
+  stopButton.removeAttribute('disabled');
+
+  colorSwitcher = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+function handleStop(e) {
+  stopButton.setAttribute('disabled', '');
+  startButton.removeAttribute('disabled');
+
+  clearInterval(colorSwitcher);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
